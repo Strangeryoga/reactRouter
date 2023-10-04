3671e7598f02907f3e7bf2c1a10e5a8cfc281498
Strangeryoga <tejasdoke4@gmail.com>
2023-10-04 23:21:47 +0530

    fix footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,7 +19,7 @@ function Footer() {
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
                             <ul className="text-gray-500 font-medium">
-                                <li className='mb-3'>
+                                <li className='mb-3.5S'>
                                     <NavLink
                                     to="/"
                                        className={({isActive}) => 
